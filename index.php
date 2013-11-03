66d77caff197a34625e335dd9e01ebd2d2c8b2a9
Fikri Rasyid <fikrirasyid@gmail.com>
2013-11-03 10:58:03 +0700

    first init, baby. done crafting basic home page

---
+++ index.php
@@ -0,0 +1,211 @@
+<html>
+<head><meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Opus</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700|Montserrat:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' href='<?php echo get_bloginfo('stylesheet_directory'); ?>/css/screen.css' type='text/css' media='all'/>
+	<!-- <link rel='stylesheet' href='http://10.0.1.212/fikrirasyid/wp-content/themes/opus/css/screen.css' type='text/css' media='all'/> -->
+	<script type="text/javascript" src="<?php echo get_bloginfo('stylesheet_directory'); ?>/js/live.js"></script>
+<!-- </head> -->
+<body>
+	<nav id="top-nav" class="wrap-outer" role="navigation">
+		<div class="wrap">
+			<h2 id="top-nav-toggle" class="menu-toggle" title="Show Menu">Show Menu</h2>
+			<div class="top-nav-container">
+				<div class="menu-top-nav-container">
+					<ul id="menu-top-nav" class="menu">
+						<li id="menu-item-955" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-955"><a href="http://fikrirasyid.com/about/">About Me</a></li>
+						<li id="menu-item-1071" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1071"><a href="http://fikrirasyid.com/contact/">Contact</a></li>
+					</ul>
+				</div>				
+			</div>
+		</div>
+	</nav>
+
+	<header id="header" class="wrap-outer">
+		<div class="wrap">
+			<h1 id="site-name"><a href="#">Fikri Rasyid</a></h1>
+			<h2 id="site-desc">I type things, therefore I am. <br>Let's get to know <a href="#">each other more</a></h2>			
+		</div>
+	</header>
+
+	<div id="page-cover">
+		<img src="<?php echo get_bloginfo('template_directory'); ?>/images/dummy/cover-photo-1.jpg" alt="">
+	</div>				
+
+<div id="main" class="site-main">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<article id="post-3448" class="post-3448 post type-post status-publish format-quote hentry category-lesson-learned tag-gtd tag-productivity">
+				<header class="entry-header">
+					<div class="entry-meta darker">
+						<ul class="entry-category">
+							<li><a href="http://fikrirasyid.com/category/lesson-learned/" title="View all posts in Lesson Learned" rel="category tag">Lesson Learned</a></li>
+						</ul>
+
+						<div class="entry-posted-on">
+							<span class="entry-date">
+								<span class="human-time">two days ago</span>
+								<span class="conventional-time">October 30, 2013</span>
+							</span>
+						</div>
+					</div>
+					
+					<div class="entry-background">
+						<div class="shadow"></div>
+						<img src="http://fikrirasyid.com/wp-content/uploads/2013/10/Phone-and-laptops-1024x576.jpg" class="background-image">
+					</div>
+				</header> 
+				<div class="entry-content">
+					<blockquote><p>If you have too many things to do, the chance is you’ll end up doing none of them.</p>
+					<p>Learn to focus and prioritize.</p></blockquote>
+					<p>- Things I’ve realized recently.</p>
+				</div> 
+				<footer class="entry-footer">
+					<span class="icon-wrap"><span class="icon"></span></span>
+					<span class="comments-link"><a href="http://fikrirasyid.com/too-many-things-to-do/#respond" title="Comment on If you have too many things to do, the chance is y…">No Response Yet. <strong>Add Yours</strong></a></span>
+				</footer> 
+			</article><!-- article -->
+
+			<article id="post-3443" class="post-3443 post type-post status-publish format-standard hentry category-lesson-learned tag-advice tag-airbnb tag-artist tag-attention tag-craftmanship tag-great-artist tag-love tag-the-great-discontent">
+				<header class="entry-header">
+					<div class="entry-meta">
+					<ul class="entry-category">
+						<li><a href="http://fikrirasyid.com/category/lesson-learned/" title="View all posts in Lesson Learned" rel="category tag">Lesson Learned</a></li>
+					</ul>
+
+					<div class="entry-posted-on">
+						<span class="author vcard">
+							<span class="entry-date" style="width: 92px;"><span class="human-time">three days ago</span>
+							<span class="conventional-time">October 28, 2013</span>
+						</span>
+					</div>
+
+					</div>
+					<a href="http://fikrirasyid.com/attention-to-details/" class="entry-featured-image" rel="bookmark">
+						<img src="http://fikrirasyid.com/wp-content/uploads/2013/10/The-guitar-1024x679.jpg" class="attachment-large wp-post-image" alt="The guitar">
+					</a>
+					
+					<h1 class="entry-title">
+						<a href="http://fikrirasyid.com/attention-to-details/" rel="bookmark">Attention To Details</a>
+					</h1>
+				</header> 
+				<div class="entry-content">
+					<p>The details differ great artist to the mediocre one.</p>
+					<p>Specifically, how does those details make his audience feels.</p>
+					<p>Feels.</p>
+					<p>How other feels.</p>
+					<p><a title="Tak Ada Logika" href="http://fikrirasyid.com/tak-ada-logika/" target="_blank">The art of making other feels is just that important</a>.</p>
+					<p style="text-align: center;">***</p>
+					<p>This thought is inspired by <a href="https://www.airbnb.com/rooms/58045" target="_blank">this AirBnB listing</a> I found after reading <a href="http://thegreatdiscontent.com/elle-luna" target="_blank">The Great Discontent interview with Elle Luna</a>. Try reading the listing’s description, the reviewers’ comment, and <a href="https://www.airbnb.com/users/show/277323" target="_blank">the host’s story of her obsession toward Joglo</a>.</p>
+					<p>I can sense love, obsession, attention, and satisfaction there.</p>
+					<p><small>Featured image used is courtesy of&nbsp;<a href="http://facebook.com/lucasboesche" target="_blank">Lucas Boesche</a> via <a href="http://unsplash.com/" target="_blank">Unsplash</a>.</small></p>
+				</div> 
+				<footer class="entry-footer">
+					<span class="icon-wrap"><span class="icon"></span></span>
+					<span class="comments-link"><a href="http://fikrirasyid.com/attention-to-details/#respond" title="Comment on Attention To Details">No Response Yet. <strong>Add Yours</strong></a></span>
+				</footer> 
+			</article>
+
+			<article id="post-3439" class="post-3439 post type-post status-publish format-standard hentry category-tech tag-apple tag-macbook-pro tag-mavericks tag-mountain-lion tag-os-x tag-upgrade">
+				<header class="entry-header">
+					<div class="entry-meta">
+						<ul class="entry-category">
+							<li><a href="http://fikrirasyid.com/category/tech/" title="View all posts in Tech" rel="category tag">Tech</a></li>
+						</ul>
+						<div class="entry-posted-on">
+							<span class="entry-date">
+								<span class="human-time">three days ago</span>
+								<span class="conventional-time">October 28, 2013</span>
+							</span>
+						</div>
+					</div>
+					<a href="http://fikrirasyid.com/upgrade-ke-os-x-10-9-mavericks/" class="entry-featured-image" rel="bookmark">
+					<img src="http://fikrirasyid.com/wp-content/uploads/2013/10/Upgrade-ke-OS-X-Mavericks-1024x746.jpeg" class="attachment-large wp-post-image" alt="Upgrade ke OS X Mavericks"> </a>
+					<h1 class="entry-title">
+						<a href="http://fikrirasyid.com/upgrade-ke-os-x-10-9-mavericks/" rel="bookmark">Upgrade Ke OS X 10.9 Mavericks</a>
+					</h1>
+				</header> 
+				<div class="entry-content">
+					<p>Akhir pekan lalu gue memutuskan untuk melakukan&nbsp;<em>clean install&nbsp;</em>sekalian meng-<em>upgrade</em> OS X di MacBook Pro gue. Alasan gue melakukan&nbsp;<em>clean install&nbsp;</em>sederhana: Macbook gue udah&nbsp;<em>bloated&nbsp;</em>banget rasanya. Sebagai perbandingan, MBP gue yang RAM-nya 8 GB masih terasa lebih&nbsp;<em>laggy&nbsp;</em>daripada iMac kantor yang RAM-nya “cuma” 4 GB. Gue pikir, udah terlalu banyak hal yang gue mampatin ke MBP ini. Karena beli MacBook Air baru masih jauh budget-nya, yaudah <em>let’s start fresh by clean-installing the MBP</em>. Resikonya punya&nbsp;<em>device&nbsp;</em>yang lambat ini terlalu berat buat gue: kalo lambat, jadi males ngapa-ngapain. Kalo males ngapa-ngapain, gue jadi ngga ngebuat apa-apa. <em>Damn scary.</em>&nbsp;<em>Fast and responsive machine is an investment</em>.</p>
+					<p>Setelah nanya-nanya di twitter dan kebantu banget dengan jawaban @<a href="https://twitter.com/fikrirasyid/status/393581973173596160" target="_blank">adhams</a>, gue memutuskan untuk mengikuti <a href="http://mashable.com/2013/10/23/clean-install-os-x-mavericks/">tutorial yang dipublikasikan oleh Mashable</a>. Caranya sederhana:</p>
+					<ol>
+					<li>Backup data penting ke HD eksternal</li>
+					<li>Download installer-nya dari App Store</li>
+					<li>Buat jadi bootable USB pake Diskmaker X</li>
+					<li>Restart, command + R, masuk ke disk utility.</li>
+					<li>Erase partisi OS X</li>
+					<li>Balik ke menu di disk utility, install new OS X</li>
+					<li>Balikin data yang penting ke HD</li>
+					</ol>
+					<p><em>Sounds pretty easy, right? Unfortunately, shit happened.</em> Semuanya lancar sampe langkah ke 4, tapi pas langkah ke 5 gue ngga nemu opsi install new OS X Mavericks. Adanya restore OS X Mountain Lion.</p>
+					<p>Ah, gue pikir belum kebaca nih bootable USB-nya. Dengan bloonnya gue nge-restart MBP + tahan <em>option</em>, ternyata bootable USB-nya ngga kebaca dan satu-satunya partisi yang bisa kebaca cuman partisi bootcamp yang ada Windowsnya. Partisi Machintosh HD udah kosong, dan partisi restore OS X 10.8 juga udah keapus.&nbsp;Wah sial iki -_-</p>
+					<p>Gue restart lagi, dan tahan command + R. Malah langsung masuk ke recovery online yang nge-download disk utility temporer (karena partisi Machintosh HD-nya udah kosong) buat nge-download dan nge-install ulang Mountain Lion.&nbsp;Gue pikir, kalo kudu nge-download Mountain Lion dulu kapan beresnya. Akhirnya gue kepikiran opsi yang relatif lebih cepet:</p>
+					<ol>
+					<li>Install ulang OS X 10.6 Snow Leopard dari DVD bawaan MBP – berhasil.</li>
+					<li>Copy installer OS X 10.9 Mavericks ke MBP terus install biasa – ternyata untuk nge-install Mavericks kudu upgrade dulu ke SL terbaru yang udah ada App Store-nya. Upgrade SL (file-nya sekitar 1 GB-an) lalu install Mavericks, berhasil</li>
+					<li>Download ulang semua App di app store. Not to mention, yang ngga terlalu penting jangan dimasukin lagi :))</li>
+					</ol>
+					<p><em>That’s how I rolled.</em> Setelah gue pikir-pikir, sebenarnya tahapannya bisa dibuat lebih efisien andaikan setelah nge-erase isi partisi, gue langsung restore si Mountain Lion yang ada di partisi hardisk. Dari situ tinggal upgrade ke Mavericks. Tapi ya sudahlah, ini jadi catatan kalau nanti lagi mau nge-<em>clean install</em>:</p>
+					<ol>
+					<li>Download installer dan copy ke desktop</li>
+					<li>Backup seluruh file ke HDD eksternal</li>
+					<li>Restart + ( Command + R), masuk disk utility, erase partisi</li>
+					<li>Restore current OS X</li>
+					<li>Upgrade ke OS X terbaru</li>
+					<li>Balikin data ke HD</li>
+					</ol>
+					<p>Omong-omong gue ngga menjamin langkah-langkah diatas bakal&nbsp;<em>problem-free&nbsp;</em>ya.&nbsp;<em>Use it at your own risk</em>.&nbsp;Gue ngga ikut bertanggung jawab jika ada yang ngikutin langkah ini dan malah jadi bermasalah. Ini sih kesimpulan hasil pengalaman gue aja.</p>
+					<p><em>Anyway</em>, hal menarik setelah upgrade: gue dari dulu belum nge-upgrade iLife karena versi bawaannya MBP ini versi yang sebelum masuk App Store. Setelah upgrade ke Mavericks, gue masuk ke App Store dan label di tombol download-nya aplikasi-aplikasi iLife ini cuman “install” alih-alih “149ribu”. Pas gue coba download + install, tetep ada popup “<em>Are you sure you want to buy?”</em>-nya sih. Ntar akhir bulan liat aja deh tagihan <em>credit card</em>-nya :))</p>
+					<p>Jadi, udah pada update OS X?</p>
+				</div> 
+				<footer class="entry-footer">
+					<span class="icon-wrap"><span class="icon"></span></span>
+					<span class="comments-link"><a href="http://fikrirasyid.com/upgrade-ke-os-x-10-9-mavericks/#respond" title="Comment on Upgrade Ke OS X 10.9 Mavericks">No Response Yet. <strong>Add Yours</strong></a></span>
+				</footer> 
+			</article>
+
+
+			<nav role="navigation" id="nav-below" class="navigation-paging">
+				<h2 class="screen-reader-text">Post navigation</h2>
+				<div class="nav-previous">
+					<a href="http://fikrirasyid.com/page/3/">Older</a>
+				</div>
+				<div class="nav-next">
+					<a href="http://fikrirasyid.com/">Newer</a>
+				</div>
+			</nav>
+
+		</div><!-- #content.site-content -->
+	</div><!-- #primary.content-area -->
+</div><!-- #main.site-main -->
+
+<footer id="colophon" class="site-footer wrap-outer" role="contentinfo">
+	<div id="secondary" class="widget-area" role="complementary">
+		<div class="wrap">
+			<aside id="nav_menu-3" class="widget widget_nav_menu">
+				<h2 class="widget-title">MY PRESENCES ELSEWHERE</h2>
+				<div class="menu-top-container">
+					<ul id="menu-top" class="menu">
+						<li id="menu-item-1821" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1821"><a href="http://twitter.com/fikrirasyid">On Twitter</a></li>
+						<li id="menu-item-1822" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1822"><a href="http://www.linkedin.com/in/fikrirasyid">On LinkedIn</a></li>
+						<li id="menu-item-1823" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1823"><a href="https://github.com/fikrirasyid">On GitHub</a></li>
+						<li id="menu-item-1830" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1830"><a href="http://soundcloud.com/fikrirasyid">On Soundcloud</a></li>
+						<li id="menu-item-1824" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1824"><a href="http://fikrirasyid.tumblr.com">On Tumblr</a></li>
+						<li id="menu-item-3121" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3121"><a href="https://www.google.com/search?q=fikri+rasyid&amp;ie=utf-8&amp;oe=utf-8&amp;aq=t">Need More? Google My Name Here</a></li>
+						</ul>
+					</div>
+			</aside>
+		</div> 
+	</div> 
+
+	<div class="site-info">
+		<div class="wrap">
+		Powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a>. This blog is written, <a href="http://fikrirasyid.com/flucid/">designed, and developed</a> by Fikri Rasyid 
+		</div> 
+	</div> 
+</footer><!-- #colophon.site-footer -->
+
+</body>
+</html>
