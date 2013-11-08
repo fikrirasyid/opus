18ace07576c9a4f9f990b3c1d81b5201ab434c15
Fikri Rasyid <fikrirasyid@gmail.com>
2013-11-09 02:14:51 +0700

    adding top-nav mechanism for showing background when scrolling

--- js/script.js
+++ js/script.js
@@ -5,8 +5,25 @@ jQuery(document).ready(function($) {
 		var item_width = item.width();
 		var content_width = $('#content .entry-content').width();
 		if( item_width > content_width ){
-			item.removeAttr('width').css({'width' : '100%'}).is('img div').removeAttr('height');
+			item.removeAttr('width').css({'width' : '100%'});
+
+			if( item.is('img div') ){
+				item.removeAttr('height');
+			}
 		}
 	}	
 	$('.entry-content img, .entry-content .wp-caption, .entry-content iframe').each(opus_normalize_media);
+
+	/**
+	 * Top Navigation Mechanism
+	 */
+	$(window).scroll(function(){
+		var window_offset = $(window).scrollTop();
+		var main_offset = $('#main').offset();
+		if( window_offset > main_offset.top ){
+			$('#top-nav').addClass('scrolled');
+		} else {
+			$('#top-nav').removeClass('scrolled');
+		}
+	});
 });
