124173602c9b24c2a39ba40592619e779e760621
Fikri Rasyid <fikrirasyid@gmail.com>
2013-11-22 06:46:22 +0700

    Correct footer credit and attribution

--- footer.php
+++ footer.php
@@ -11,7 +11,13 @@
 
 		<div class="site-info">
 			<div class="wrap">
-			Powered by <a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a>. This blog is written, <a href="http://fikrirasyid.com/flucid/">designed, and developed</a> by Fikri Rasyid 
+				<?php
+					printf(
+						__( 'Powered by %1$s and %2$s', 'opus' ),
+						'<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform">WordPress</a>',
+						'<a href="http://fikrirasyid.com/wordpress-themes/opus/">Opus Theme</a>'
+					);
+				?>			
 			</div> 
 		</div> 
 	</footer><!-- #colophon.site-footer -->
