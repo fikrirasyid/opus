4daf4edc9cc3477351d3fcb4ce02a4ddb9dbe8d3
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-05 10:11:30 +0700

    content-video for video post format
    Early version of specific template for video post format. It only
    accepts self hosted video for the time being. Running out of time now,
    gonna revisit this issue later today

---
+++ content-video.php
@@ -0,0 +1,24 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<div class="entry-meta">
+			<?php opus_the_category(); ?>
+
+			<?php opus_posted_on(); ?>
+		</div>					
+		<?php 
+			$video = get_post_meta( get_the_ID(), '_format_video_embed', true );
+			if( $video ):
+				echo "<video controls><source src='$video'></source></video>";
+			endif; 
+		?>
+	</header>
+	<div class="entry-content">
+		<?php the_content( __( 'Continue Reading &rarr;', 'opus' ) ); ?>
+	</div> 
+	<footer class="entry-footer">
+		<span class="icon-wrap"><span class="icon"></span></span>
+		<span class="comments-link">
+			<?php opus_comments_popup_link(); ?>
+		</span>
+	</footer> 
+</article>
