79e2a995f92d35c5763e0cd6d880ba5579fb2f8f
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-24 06:52:19 +0700

    Fixing search form bug on 404 page

--- 404.php
+++ 404.php
@@ -26,7 +26,7 @@
 					<p><?php _e( 'We\'re sorry, the page you\'re looking for does not exist', 'opus' ); ?></p>
 					<p><?php printf( __( 'We suggest you to check our <a href="%1$s" title="Back to homepage">homepage</a> or try to search what you\'re looking for using this search box below:', 'opus' ), home_url() ); ?></p>
 
-					<form role="search" method="get" id="searchform" class="searchform" action="http://localhost/fikrirasyid/">
+					<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
 						<label class="screen-reader-text" for="s">Search for:</label>
 						<input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Type keywords and hit enter', 'opus' ); ?>" style="margin:0 0 15px;">
 						<input type="submit" id="searchsubmit" value="Search" style="margin: 0;">
