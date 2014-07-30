84d07160917d3043c0e74a4e7c4dda6bf8895c7d
Fikri Rasyid <fikrirasyid@gmail.com>
2014-07-30 07:58:01 +0700

    Adding aside template

---
+++ content-aside.php
@@ -0,0 +1,26 @@
+<?php get_template_part( 'article', 'time' ); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<div class="entry-meta">
+			<?php opus_the_category(); ?>
+
+			<?php opus_posted_on(); ?>
+		</div>
+
+		<?php if( has_post_thumbnail() ): ?>
+		<a href="<?php the_permalink(); ?>" class="entry-featured-image" rel="bookmark">
+			<?php the_post_thumbnail( 'featured' ); ?>
+		</a>
+		<?php endif; ?>				
+	</header> 
+	<div class="entry-content">
+		<?php the_content( __( 'Continue Reading &rarr;', 'opus' ) ); ?>
+	</div> 
+	<footer class="entry-footer">
+		<span class="icon-wrap"><span class="icon"></span></span>
+		<span class="comments-link">			
+			<?php opus_comments_popup_link(); ?>
+		</span>
+	</footer> 
+</article>
