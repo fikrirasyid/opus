8a51f784e0d29216882f6b1a335c4c52ebfaec4d
Fikri Rasyid <fikrirasyid@gmail.com>
2014-08-04 10:20:23 +0700

    Adding content-none template
    Adding content-none template for not found content
    404 template now uses content-none template too

--- 404.php
+++ 404.php
@@ -6,39 +6,7 @@
 			</div>				
 			<div id="content" class="site-content" role="main">
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header class="entry-header">
-					<div class="entry-meta">
-						<ul class="entry-category">
-							<li><?php _e( 'Error 404', 'opus' ); ?></li>
-						</ul>
-						
-						<div class="entry-posted-on">
-							Whoops.
-						</div>
-					</div>
-					
-					<h1 class="entry-title">
-						<a href="<?php the_permalink(); ?>" rel="bookmark"><?php _e( 'Not Found', 'opus' ); ?></a>
-					</h1>
-				</header> 
-				<div class="entry-content">
-					<p><?php _e( 'We\'re sorry, the page you\'re looking for does not exist', 'opus' ); ?></p>
-					<p><?php printf( __( 'We suggest you to check our <a href="%1$s" title="Back to homepage">homepage</a> or try to search what you\'re looking for using this search box below:', 'opus' ), home_url() ); ?></p>
-
-					<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
-						<label class="screen-reader-text" for="s">Search for:</label>
-						<input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Type keywords and hit enter', 'opus' ); ?>" style="margin:0 0 15px;">
-						<input type="submit" id="searchsubmit" value="Search" style="margin: 0;">
-					</form>					
-				</div> 				
-				<footer class="entry-footer">
-					<span class="icon-wrap"><span class="icon"></span></span>
-					<span class="comments-link">			
-						<?php _e( 'Commentless', 'opus' ); ?>
-					</span>
-				</footer> 
-			</article>
+			<?php get_template_part( 'content', 'none' ); ?>
 
 			</div><!-- #content.site-content -->
 		</div><!-- #primary.content-area -->
---
+++ content-none.php
@@ -0,0 +1,37 @@
+<article id="post-0" <?php post_class(); ?>>
+	<header class="entry-header">
+		<div class="entry-meta">
+			<ul class="entry-category">
+				<li><?php _e( 'Not Found', 'opus' ); ?></li>
+			</ul>
+			
+			<div class="entry-posted-on">
+				Whoops.
+			</div>
+		</div>
+		
+
+	</header> 
+	<div class="entry-content">
+		
+		<?php if( is_search() ): ?>
+			<?php printf( __( 'There is no article that relates with keyword <strong>%s</strong>', 'opus' ), $s ); ?>
+		<?php else: ?>
+			<p><?php _e( 'We are sorry, the page you are looking for does not exist', 'opus' ); ?></p>
+		<?php endif; ?>
+
+		<p style="margin-top: 20px; font-size: .85em;"><?php printf( __( 'We suggest you to check the <a href="%1$s" title="Back to homepage">homepage</a> or try to search what you\'re looking for using this search box below:', 'opus' ), home_url() ); ?></p>
+
+		<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
+			<label class="screen-reader-text" for="s">Search for:</label>
+			<input type="text" value="" name="s" id="s" placeholder="<?php _e( 'Type keywords and hit enter', 'opus' ); ?>" style="margin:0 0 15px;">
+			<input type="submit" id="searchsubmit" value="Search" style="margin: 0;">
+		</form>					
+	</div> 				
+	<footer class="entry-footer">
+		<span class="icon-wrap"><span class="icon"></span></span>
+		<span class="comments-link">			
+			<?php _e( 'Commentless', 'opus' ); ?>
+		</span>
+	</footer> 
+</article>
