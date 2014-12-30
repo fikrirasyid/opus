33762100307f635081d65236e7d90b6831d6e602
Fikri Rasyid <fikrirasyid@gmail.com>
2014-12-31 06:05:03 +0700

    Better civil footnote support
    In infinite scroll situation, the sliding instead of jumping mechanism
    will still work now

--- js/script.js
+++ js/script.js
@@ -234,8 +234,11 @@ jQuery(document).ready(function($) {
 		});
 	}
 
-	// Footnote Support: Slide the window instead of jumping it
-	$('a[rel="footnote"], a.backlink').click(function(e){
+	/**
+	* Civil Footnotes Support
+	* Slide the window instead of jumping it
+	*/
+	$('#main').on( 'click', 'a[rel="footnote"], a.backlink', function(e){
 		e.preventDefault();
 		var target_id = $(this).attr('href');
 		var respond_offset = $(target_id).offset();
@@ -243,8 +246,8 @@ jQuery(document).ready(function($) {
 		$('html, body').animate({
 			scrollTop : respond_offset.top - 90
 		});
-	});	
-
+	});		
+	
 	// Show conventional time when user hovering .entry-date
 	$('#content').on( 'mouseenter', '.entry-meta', function(){
 		$(this).find('.on-load').hide();
